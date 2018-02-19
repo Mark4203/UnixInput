5e477244a2da0a5738dfd3dea3d1367756ea07cf
Mark Kurbatskiy <mkurbatsky3432@gmail.com>
2018-02-19 14:47:20 -0500

    new stuff

---
+++ DataTypes.java
@@ -0,0 +1,22 @@
+public class DataTypes{
+ public static void main (String [] args) {
+ int a = 10;
+ int b = 20;
+ int c = 30;
+ 
+ sum = a + C;
+ difference = b - a;
+ product = c * a;
+ quoutient = b / a;
+ remainder  = d % c;
+
+ System.out.println("A is " + a);
+ System.out.println("B is " + b);
+ System.out.println("C is " + c);
+ System.out.println("The sum is:" + sum);
+ System.out.println("The difference is:" + difference);
+ System.out.println("The product is:" + product); 
+ System.out.println("The quoutient is:" + quoutient);
+ System.out.println("The remainder is:" + remainder);
+ }
+}
